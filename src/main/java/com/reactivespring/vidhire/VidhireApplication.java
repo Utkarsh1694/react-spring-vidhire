23e9a618b1ccef18e1fe1be87437b48181f59d2b
Utkarsh1694 <utksri16@gmail.com>
2020-04-21 21:54:29 +0530

    First Commit

---
+++ src/main/java/com/reactivespring/vidhire/VidhireApplication.java
@@ -0,0 +1,13 @@
+package com.reactivespring.vidhire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidhireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidhireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactivespring/vidhire/VidhireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivespring.vidhire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidhireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
